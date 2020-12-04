24b92e98fa5cfe0c47e3624667e84f8a6a59d34f
Juanma <jmayobel@alumnos.unex.es>
2020-12-04 20:44:42 +0100

    prueba2

---
+++ pruebaclase2.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class pruebaclase2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class pruebaclase2
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class pruebaclase2
+     */
+    public pruebaclase2()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
