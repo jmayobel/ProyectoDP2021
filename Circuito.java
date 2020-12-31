1390f7d0200776150f511ffe85ae044f36fb69a4
Miguel Medina Cantos <mimedinac@alumnos.unex.es>
2020-12-31 15:55:32 +0100

    interface

--- Circuito.java
+++ Circuito.java
@@ -7,8 +7,11 @@
  */
 public interface Circuito
 {
-    
-   
+    public String getNombreCircuito ();
+    public Complejidad getComplejidad ();
+    public Distancia getDistancia ();
+    public double getValorComplejidad ();
+    public double getValorDistancia ();
     
     
     
