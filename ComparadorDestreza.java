73273d854a2218adae7d6f3db78420e00f8b2317
César Vázquez Lázaro <62475219+ITheGamerBossI@users.noreply.github.com>
2021-01-08 12:38:57 +0100

    Extendida el comparador con la clase abstracta

--- ComparadorDestreza.java
+++ ComparadorDestreza.java
@@ -5,17 +5,14 @@ import java.util.Comparator;
  * @author (Miguel)
  * @version (a version number or a date)
  */
-public class ComparadorDestreza implements Comparator<PilotoInterfaz>
+public class ComparadorDestreza extends ComparadorPiloto
 {
      public int compare(PilotoInterfaz p1, PilotoInterfaz p2){
         if(p1.getDestreza()==p2.getDestreza())
-            return 0;
+            return new ComparadorTotalPuntos().compare(p1, p2);
         else if(p1.getDestreza()>p2.getDestreza())
             return 1;
         else
             return -1;
-    }
-    
-    
-    
+    }    
 }
