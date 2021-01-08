8aaa343a2b87175e76e8d12b0c01882fbb2410cf
César Vázquez Lázaro <62475219+ITheGamerBossI@users.noreply.github.com>
2021-01-08 12:37:56 +0100

    Extendida el comparador con la clase abstracta

--- ComparadorConcentracion.java
+++ ComparadorConcentracion.java
@@ -3,15 +3,14 @@ import java.util.Comparator;
 /**
  * Write a description of class ComparadorConcentracion here.
  *
- * @author (Miguel)
- * @version (a version number or a date)
+ * @author MIGUEL MEDINA
+ * @version 0.0
  */
-public class ComparadorConcentracion implements Comparator<PilotoInterfaz>
+public class ComparadorConcentracion extends ComparadorPiloto
 {
-    // instance variables - replace the example below with your own
   public int compare(PilotoInterfaz p1, PilotoInterfaz p2){
         if(p1.getTiempoConcentracion()==p2.getTiempoConcentracion())
-            return 0;
+            return new ComparadorDestreza().compare(p1, p2);
         else if(p1.getTiempoConcentracion()>p2.getTiempoConcentracion())
             return 1;
         else
