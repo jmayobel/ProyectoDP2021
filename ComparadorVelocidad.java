d382aab090c65f4ca8e09353b14470db3599c945
César Vázquez Lázaro <62475219+ITheGamerBossI@users.noreply.github.com>
2021-01-08 12:21:00 +0100

    Prueba de Comparador
    Nueva versión del Comparator

--- ComparadorVelocidad.java
+++ ComparadorVelocidad.java
@@ -5,15 +5,14 @@ import java.util.Comparator;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class ComparadorVelocidad implements Comparator<CocheInterfaz>
+public class ComparadorVelocidad extends ComparadorCoche
 {
     public int compare(CocheInterfaz c1, CocheInterfaz c2){
         if(c1.getValorVelocidad()==c2.getValorVelocidad())
-            return 0;
+            return new ComparadorCombustible().compare(c1, c2);
         else if(c1.getValorVelocidad()>c2.getValorVelocidad())
             return 1;
         else
             return -1;
-    } 
-        
-    }
+    }         
+}
