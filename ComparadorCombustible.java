416d694f6d9a0b8e162c6f7e292e7b89cc08a9cb
César Vázquez Lázaro <62475219+ITheGamerBossI@users.noreply.github.com>
2021-01-09 15:47:25 +0100

    Añadida la documentación javaDoc

--- ComparadorCombustible.java
+++ ComparadorCombustible.java
@@ -1,12 +1,19 @@
 import java.util.Comparator;
 /**
- * Write a description of class CombustibleComparador here.
+ * Comparador de Coches que permite la ordenación según el combustible del mismo. 
  * 
  * @author CESAR VAZQUEZ LAZARO 
- * @version 0.0
+ * @version 0.1
  */
 public class ComparadorCombustible extends ComparadorCoche
 {
+    /**
+    * Comparador de Coches en función del combustible.
+    * En caso de empate, comparará por el nombre del coche.
+    * 
+    * @param c1 Coche a comparar
+    * @param c2 Coche a ser comparado
+    */
     public int compare(CocheInterfaz c1, CocheInterfaz c2){
         if(c1.getValorcombustible()==c2.getValorcombustible())
             return (c1.getNombreCoche().compareTo(c2.getNombreCoche()));
@@ -16,4 +23,3 @@ public class ComparadorCombustible extends ComparadorCoche
             return -1;
     }
 }
-
