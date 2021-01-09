b9c409cfa06117839f3aadba6c59b9f47186a240
César Vázquez Lázaro <62475219+ITheGamerBossI@users.noreply.github.com>
2021-01-09 15:15:21 +0100

    Añadida la documentación javaDoc

--- ComparadorTotalPuntos.java
+++ ComparadorTotalPuntos.java
@@ -1,13 +1,19 @@
-
 import java.util.Comparator;
 /**
- * Write a description of class ComparadorTotalPuntos here.
+ * Comparador de Pilotos que permite la ordenación según el total de puntos del mismo. 
  * 
  * @author CESAR VAZQUEZ LAZARO 
- * @version 0.0
+ * @version 0.1
  */
 public class ComparadorTotalPuntos extends ComparadorPiloto
 {
+    /**
+     * Comparador de Pilotos en función del total de puntos.
+     * En caso de empate, comparará por el nombre del piloto.
+     * 
+     * @param p1 Piloto a comparar
+     * @param p2 Piloto a ser comparado
+     */
     public int compare(PilotoInterfaz p1, PilotoInterfaz p2){
         if(p1.getPuntosTotales()==p2.getPuntosTotales())
             return (p1.getNombre().compareTo(p2.getNombre()));
