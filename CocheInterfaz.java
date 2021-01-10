b0e63052118cb133bb9e27180ecbe11603b1bb28
jmayobel <jmayobel@alumnos.unex.es>
2021-01-10 16:21:25 +0100

    Coche añadidos

--- CocheInterfaz.java
+++ CocheInterfaz.java
@@ -1,16 +1,31 @@
-/**
+;/**
  * Write a description of class Coche here.
  * 
  * @author Juan Manuel Mayo Beltrán
  * @version (a version number or a date)
  */
 public interface CocheInterfaz {
-	public String getNombreCoche();
-	public Velocidad getvelocidad();
-	public double getValorVelocidad();
-	public Combustible getcombustible(); 
-	public double getValorcombustible();
-	public double getCombustibleUsado(PilotoInterfaz piloto, Circuito circuito);
-	public double getTiempo(PilotoInterfaz piloto, Circuito circuito);
-	public double getVelocidadReal(PilotoInterfaz piloto, Circuito circuito);
+    public void setnombreCoche(String nombre);
+
+    public void setvelocidad(Velocidad velocidad);
+
+    public void setCombustible(Combustible combustible);
+
+    public void setCombustibleUsado(double combustibleU);
+
+    public String getNombreCoche();
+
+    public Velocidad getvelocidad();
+
+    public double getValorVelocidad();
+
+    public Combustible getcombustible(); 
+
+    public double getValorcombustible();
+
+    public double getCombustibleUsado(PilotoInterfaz piloto, Circuito circuito);
+
+    public double getTiempo(PilotoInterfaz piloto, Circuito circuito);
+
+    public double getVelocidadReal(PilotoInterfaz piloto, Circuito circuito);
 }
