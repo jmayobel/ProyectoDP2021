c7505275faa6aeb1e7976ed1b7bd654771e9adac
César Vázquez Lázaro <62475219+ITheGamerBossI@users.noreply.github.com>
2021-01-10 12:38:33 +0100

    Añadida un ArrayList que guarda los pilotos preparados para la carrera

--- EscuderiaRally.java
+++ EscuderiaRally.java
@@ -7,9 +7,9 @@ import java.util.*;
  */
 public class EscuderiaRally implements EscuderiaInterfaz
 {
-
     //Variables de la clase EscuderiaRally:
     private String nomEscuderia;                            //Nombre de la escudería
+    private ArrayList <PilotoInterfaz> pilotosCarrera;      //Lista con los pilotos que se enviarán a la carrera
     private ArrayList <PilotoInterfaz> ListaPilotos;        //Lista de los pilotos de la escudería        
     private ArrayList <CocheInterfaz> ListaCoches;          //Lista de los coches de la escudería
     private Comparator<PilotoInterfaz> compPilotos;         //Comparador que ordenará la lista de pilotos
@@ -24,6 +24,7 @@ public class EscuderiaRally implements EscuderiaInterfaz
      */
     public EscuderiaRally(String nomEscuderia, Comparator<PilotoInterfaz> compPilotos, Comparator<CocheInterfaz> compCoches){
         this.nomEscuderia = nomEscuderia;
+        this.pilotosCarrera = new ArrayList <PilotoInterfaz>();
         this.ListaPilotos = new ArrayList <PilotoInterfaz> ();
         this.ListaCoches = new ArrayList <CocheInterfaz> ();
         this.compPilotos = compPilotos;
@@ -38,6 +39,14 @@ public class EscuderiaRally implements EscuderiaInterfaz
      */
     public String getNombre(){        
         return this.nomEscuderia;        
+    }
+    /**
+     * Devuelve la lista de pilotos preparados para la carrera.
+     * 
+     * @return Lista de Pilotos que están listos para correr la carrera   
+     */
+    public ArrayList getPilotosCarrera(){ 
+        return this.pilotosCarrera;     
     }
     /**
      * Devuelve la lista de pilotos
@@ -88,6 +97,14 @@ public class EscuderiaRally implements EscuderiaInterfaz
         this.compCoches = comparador;
     }
     /**
+     * Añade un piloto a la lista de pilotos que irán a la carrera
+     * 
+     * @param piloto Piloto a insertar   
+     */
+    public void addPilotosCarrera(PilotoInterfaz piloto){          
+        this.pilotosCarrera.add(piloto);        
+    }
+    /**
      * Añade un piloto a la lista de pilotos
      * 
      * @param piloto Piloto a insertar   
@@ -102,6 +119,22 @@ public class EscuderiaRally implements EscuderiaInterfaz
      */
     public void addListaCoches(CocheInterfaz coche){        
         this.ListaCoches.add(coche);    
+    }
+    /**
+     * Elimina un piloto de la lista de pilotos que irán a la carrera
+     * 
+     * @param buscado Piloto a eliminar   
+     */    
+    public void eliminarPilotosCarrera(PilotoInterfaz buscado){
+        Iterator<PilotoInterfaz> it = this.pilotosCarrera.iterator(); //Inicializamos el Iterator
+        boolean enc = false;
+        while(it.hasNext() && !enc){
+            PilotoInterfaz buscar = it.next();
+            if(buscar.getNombre().equals(buscado)){
+                enc = true;
+                it.remove();
+            }     
+        }
     }
     /**
      * Elimina un piloto de la lista de pilotos
@@ -133,6 +166,16 @@ public class EscuderiaRally implements EscuderiaInterfaz
                 enc = true;
                 it.remove();
             }     
+        }
+    }
+    /**
+     * Vacía la lista de pilotos que irán a la carrera.
+     */
+    public void limpiarPilotosCarrera(){
+        Iterator<PilotoInterfaz> it = this.pilotosCarrera.iterator(); //Inicializamos el Iterator
+        while(it.hasNext()){
+            PilotoInterfaz eliminar = it.next();
+            it.remove();     
         }
     }
     /**
@@ -170,15 +213,16 @@ public class EscuderiaRally implements EscuderiaInterfaz
         }
         return puntostotales;   
     }
-
-     @Override
+ 
+    //toString()
+    @Override
     public String toString(){
         return getNombre();
     }
 
     //hashCode()
     @Override
-    public int hashCode(){  //No estoy seguro de que así
+    public int hashCode(){  //No estoy seguro de que sea así
         int result = 17;
         result = 7 * result + getNombre().hashCode();
         result = 13 * result + getListaPilotos().hashCode();
