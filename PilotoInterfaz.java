80cbe8908b02d283cebb8c82723395a0527022cd
César Vázquez Lázaro <62475219+ITheGamerBossI@users.noreply.github.com>
2021-01-10 19:20:58 +0100

    Añadidos los métodos para la carrera

--- PilotoInterfaz.java
+++ PilotoInterfaz.java
@@ -2,7 +2,7 @@
  * PilotoInterfaz declara los métodos que se usarán en la clase PilotoAbstracto 
  * 
  * @author CESAR VAZQUEZ LAZARO 
- * @version 0.2
+ * @version 0.3
  */
 
 public interface PilotoInterfaz{
@@ -11,15 +11,19 @@ public interface PilotoInterfaz{
     public Concentracion getConcentracion();
     public double getTiempoConcentracion();
     public boolean getDescalificado();
+    public int getAbandonos();
     public abstract double getDestreza();
     public void setCoche(Coche coche);
     public void descalificar();
+    public void abandonar();
     public void añadirResultados(Circuito circuito, double tiempo, int puntos);
+    public double buscarResultado(Circuito buscado);
     public int getTamañoResultados();
     public int getPuntosTotales();
     public void mostrarResultados();
     public void eliminarResultado(Circuito buscado);
     public void limpiarResultados();
+    public void correrCarrera(Circuito circuito);
     public String toString();
     public int hashCode();
 }
