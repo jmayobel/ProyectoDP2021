3a5e1682821d55f0193a0419e58d3c2f52dcb26b
jmayobel <jmayobel@alumnos.unex.es>
2021-06-18 13:01:01 +0200

    Cambios JavaDoc

--- Resultados.java
+++ Resultados.java
@@ -35,50 +35,49 @@ public class Resultados {
      * Devuelve el tiempo en el que el piloto ha terminado la carrera. Si el tiempo es negativo,
      * indica los minutos que le han faltado para terminarlo
      *
-     * @return  Tiempo de carrera
+     * @return Tiempo de carrera
      */
     public double getTiempoResultados() {
         return this.tiempo;
     }
 
     /**
-     * Establece el tiempo en el que el piloto acabó la carrera. Si el tiempo es negativo,
+     * Establece el tiempo en el que el piloto acabo la carrera. Si el tiempo es negativo,
      * indica los minutos que le han faltado para terminarlo.
      *
-     * @param tiempo Tiempo en el que acabó la carrera
+     * @param tiempo Tiempo en el que acabo la carrera
      */
     public void setTiempoResultados(double tiempo) {
         this.tiempo = tiempo;
     }
 
     /**
-     * Devuelve los puntos ganados en la carrera.
+     * Devuelve los puntos ganados en la carrera
      *
-     * @return puntos Puntos obtenidos
+     * @return Puntos obtenidos
      */
     public int getPuntos() {
         return this.puntos;
     }
 
     /**
-     * Establece los puntos ganados en esa carrera.
+     * Establece los puntos ganados en esa carrera
      *
-     * @param puntos Puntos obtenidos en la carrera.
+     * @param puntos Puntos obtenidos en la carrera
      */
     public void setPuntos(int puntos) {
         this.puntos = puntos;
     }
 
     /**
-     * Muestra por pantalla la información de la clase.
+     * Muestra por pantalla la informacion de la clase
      *
-     * @return String Concatenación con la información de la clase.
+     * @return Concatenacion con la informacion de la clase
      */
     @Override
     public String toString() {
         return getClass().getSimpleName() +
                 "\n Tiempo Recorrido: " + getTiempoResultados() +
                 "\n Puntos; " + getPuntos();
-
     }
 }
